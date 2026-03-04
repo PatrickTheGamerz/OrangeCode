ca38005b72b0618f384ebc94c0b8d255bb517705
PatrickTheGamerz <brolyyy50@gmail.com>
2026-03-04 20:04:29 +0100

    Update data.js

--- data.js
+++ data.js
@@ -19,169 +19,59 @@ let gameState = "title";
 let currentMenuIndex = 0;
 
 const monsterList = [
-    { id: "dummy",       name: "DUMMY",            reqLV: 1 },
-    { id: "mad_dummy",   name: "MAD DUMMY",        reqLV: 2 },
-    { id: "toriel",      name: "TORIEL",           reqLV: 3 },
-    { id: "papyrus",     name: "PAPYRUS",          reqLV: 5 },
-    { id: "p_sans",      name: "PACIFIST SANS",    reqLV: 8 },
-    { id: "us_sans",     name: "UNDERSWAP SANS",   reqLV: 9 }
+    { id: "toriel",      name: "TORIEL",           reqLV: 1 },
+    { id: "papyrus",     name: "PAPYRUS",          reqLV: 1 }
 ];
 
 const shopData = {
-    weapon: [
-        { name: "STICK", cost: 0, at: 0 },
-        { name: "TOY KNIFE", cost: 20, at: 3 },
-        { name: "TOUGH GLOVE", cost: 40, at: 5 }
-    ],
-    armor: [
-        { name: "BANDAGE", cost: 0, df: 0 },
-        { name: "FADED RIBBON", cost: 25, df: 3 },
-        { name: "MANLY BANDANNA", cost: 45, df: 5 }
-    ],
-    heal: [
-        { name: "MONSTER CANDY", cost: 5, heal: 10 },
-        { name: "SPIDER DONUT", cost: 15, heal: 12 },
-        { name: "BUTTERSCOTCH PIE", cost: 60, heal: 99 }
-    ]
+    weapon: [{ name: "STICK", cost: 0, at: 0 }],
+    armor: [{ name: "BANDAGE", cost: 0, df: 0 }],
+    heal: [{ name: "MONSTER CANDY", cost: 5, heal: 10 }]
 };
 
 const enemies = [
-    {
-        id: "dummy",
-        name: "DUMMY",
-        maxHP: 30, AT: 1, DF: 1, gold: 3, goldMercy: 5, exp: 2,
-        spareTalks: 1, 
-        soulType: "red",
-        pattern: "dummyNone",
-        patterns: ["dummyNone"],
-        openingText: "* You encountered the Dummy.",
-        checkAT: 0, checkDF: 0,
-        checkDesc: "* A cotton heart and a button eye.\n* You are the apple of my eye.",
-        actOptions: ["CHECK", "TALK"],
-        talkTexts: ["* You talk to the DUMMY.\n* ...\n* It doesn't seem much for\n  conversation.\n* DUMMY is happy with you!"],
-        turnDialogues: ["..."]
-    },
-    {
-        id: "mad_dummy",
-        name: "MAD DUMMY",
-        maxHP: 300, AT: 5, DF: 100, // High DEF so you outlast him
-        gold: 15, goldMercy: 20, exp: 10,
-        spareTalks: 99, 
-        surviveTurns: 6, // Gives up after 6 turns
-        soulType: "red",
-        pattern: "dummyAim",
-        patterns: ["dummyAim", "dummyAimBurst", "dummyAimFast"],
-        openingText: "* Mad Dummy blocks the way!",
-        checkAT: 7, checkDF: "YES",
-        checkDesc: "* Because they're a ghost, physical\n  attacks will fail.",
-        actOptions: ["CHECK", "TALK", "STARE"],
-        talkTexts: ["* You talk to the MAD DUMMY.\n* It pretends not to hear you."],
-        turnDialogues: [
-            "FOOLS!\nDUMMIES!\nDUMMIES!\nDUMMIES!",
-            "I'LL DEFEAT\nYOU AND\nTAKE YOUR\nSOUL!",
-            "WHY DO\nPEOPLE\nLIKE YOU\nEXIST?!",
-            "ARGH!!\nJUST DIE\nALREADY!",
-            "ENOUGH!\nMAGIC\nMISSILES!",
-            "N-NO!\nOUT OF\nAMMO?!"
-        ]
-    },
     {
         id: "toriel",
         name: "TORIEL",
         maxHP: 440, AT: 10, DF: 4, gold: 40, goldMercy: 50, exp: 30,
         spareTalks: 99, 
-        spareTurnsRequirement: 12, // Needs 12 SPARE actions
+        spareTurnsRequirement: 5, // Shortened for testing
         soulType: "red",
         pattern: "torielHandFire",
         patterns: ["fallingFire", "torielSideFire", "torielWaveFire", "torielHandFire"],
-        openingText: "* Toriel blocks the way!",
+        openingText: ["* Toriel blocks the way!"],
         checkAT: 80, checkDF: 80,
         checkDesc: "* Knows best for you.",
         actOptions: ["CHECK", "TALK"],
-        talkTexts: ["* You couldn't think of any\n  conversation topics."],
+        talkTexts: [["* You couldn't think of any\n  conversation topics."]],
         turnDialogues: [
-            "...", "...", "...", 
-            "What are\nyou doing?", 
-            "Attack or\nrun away!", 
-            "What are\nyou proving\nthis way?", 
-            "Fight me\nor leave!", 
-            "Stop it.", 
-            "Stop\nlooking at\nme that\nway.", 
-            "Go away!", 
-            "...", "..."
-        ],
-        betrayalText: "* You... at my most vulnerable\n  moment...\n* To think I was worried you\n  wouldn't fit in out there...\n* Ha... ha..."
+            ["..."], ["What are\nyou doing?"], ["Attack or\nrun away!"], ["Fight me\nor leave!"], ["Stop it."]
+        ]
     },
     {
         id: "papyrus",
         name: "PAPYRUS",
         maxHP: 680, AT: 8, DF: 2, gold: 50, goldMercy: 60, exp: 40,
         spareTalks: 99,
-        surviveTurns: 14, // Spares you after 14 turns
+        surviveTurns: 6, // Shortened for testing
         soulType: "blue",
+        fontClass: "papyrus-font", // Applies the custom font!
         pattern: "bonesHorizontal",
-        patterns: ["bonesHorizontal", "boneRain", "sideBones", "blueAttackPhase"],
-        openingText: "* Papyrus blocks the way!",
+        patterns: ["bonesHorizontal", "bonesHorizontalHigh", "papyrusJump"],
+        openingText: ["* Papyrus blocks the way!"],
         checkAT: 8, checkDF: 2,
         checkDesc: "* He likes to say: 'Nyeh heh heh!'",
         actOptions: ["CHECK", "FLIRT", "INSULT"],
         talkTexts: [],
+        // Arrays inside arrays allow multi-box dialogue!
         turnDialogues: [
-            "YOU'RE BLUE\nNOW!", 
-            "NYEH HEH\nHEH!", 
-            "HMM, YOU ARE\nA TOUGH ONE!", 
-            "BUT I AM\nTOUGHER!", 
-            "BEHOLD MY\nMAGNIFICENT\nBONES!", 
-            "WOW, YOU'RE\nSTILL HERE?", 
-            "HAVE YOU\nCONSIDERED\nSPAGHETTI?",
-            "NO? MORE\nBONES THEN!",
-            "MY SPECIAL\nATTACK IS\nSOON!", 
-            "ARE YOU\nREADY?", 
-            "HERE IT\nCOMES!", 
-            "WHAT?!\nWHERE IS MY\nSPECIAL\nATTACK?", 
-            "THAT DOG\nSTOLE IT!", 
-            "WELL, HERE'S\nA NORMAL\nATTACK!"
-        ]
-    },
-    {
-        id: "p_sans",
-        name: "SANS",
-        maxHP: 1, AT: 1, DF: 1, gold: 0, goldMercy: 0, exp: 0,
-        spareTalks: 0,
-        surviveTurns: 99,
-        soulType: "red",
-        pattern: "dummyNone",
-        patterns: ["dummyNone"],
-        openingText: "* Sans is asleep.",
-        checkAT: 1, checkDF: 1,
-        checkDesc: "* The easiest enemy.\n* Can only deal 1 damage.",
-        actOptions: ["CHECK", "TALK"],
-        talkTexts: ["* You try to talk.\n* Zzzzz..."],
-        turnDialogues: ["Zzz..."]
-    },
-    {
-        id: "us_sans",
-        name: "US SANS",
-        maxHP: 680, AT: 12, DF: 5, gold: 50, goldMercy: 60, exp: 40,
-        spareTalks: 99,
-        surviveTurns: 8,
-        soulType: "blue",
-        pattern: "fastBones",
-        patterns: ["fastBones", "mixedBones", "boneRain", "sansSideSpam"],
-        openingText: "* MWEH HEH HEH!\n* I, THE MAGNIFICENT SANS,\n  WILL CAPTURE YOU!",
-        checkAT: 12, checkDF: 5,
-        checkDesc: "* Extremely energetic.\n* Loves tacos.",
-        actOptions: ["CHECK", "FLIRT", "CHALLENGE"],
-        talkTexts: [""],
-        turnDialogues: [
-            "MWEH HEH\nHEH!",
-            "BEHOLD MY\nBLUE\nATTACK!",
-            "YOU'RE VERY\nGOOD AT\nJUMPING!",
-            "BUT CAN\nYOU DODGE\nTHIS?!",
-            "TACOS ARE\nTHE BEST\nFOOD!",
-            "I ALMOST\nFEEL BAD\nFOR YOU!",
-            "ALPHY WILL\nBE SO\nPROUD!",
-            "READY FOR\nMY ULTIMATE\nATTACK?!"
+            ["YOU'RE BLUE\nNOW!", "THAT'S MY\nATTACK!"], 
+            ["NYEH HEH\nHEH!"], 
+            ["HMM, YOU ARE\nA TOUGH ONE!"], 
+            ["MY <span class='red-text'>SPECIAL\nATTACK</span> IS\nSOON!", "ARE YOU\nREADY?"], 
+            ["HERE IT\nCOMES!"], 
+            // The dog scene!
+            ["WHAT?!", "WHERE IS MY\n<span class='red-text'>SPECIAL\nATTACK</span>?", "THAT DOG\nSTOLE IT!", "WELL, HERE'S\nA <span class='red-text'>NORMAL\nATTACK</span>!"]
         ]
     }
 ];
